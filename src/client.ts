d36fd9d372448b3704edc610aa97e6ffb4f44537
heran <1187228451@qq.com>
2022-03-02 12:14:49 +0800

    finish 1.2

--- src/client.ts
+++ src/client.ts
@@ -1,28 +1,31 @@
-const http = require("http")
+import * as  http from "http"
+
 /**
  * Http的客户端
  */
 export class HttpClient {
-    private port: number;
-    constructor(port: number) {
-        this.port = port;
-    }
+    constructor() { }
 
-    private getmessage() {
-        http.get(`http://localhost:${this.port}/helloworld`, (res) => {
-            console.log(`Got response: ${res.statusCode}`);
+    /**
+     * 启动客户端
+     */
+    public start() {
+        http.get(`http://localhost:3000/helloworld`, res => {
+            var html = '';
+            res.on('data', data => {
 
-            // consume response body
-            res.resume();
+                html += data;
+            });
+            res.on('end', () => {
+
+                console.log(html);
+            })
         }).on('error', (e) => {
-        console.log(`Got error: ${e.message}`);
+            console.log(`Got error: ${e.message}`);
         });
-    
     }
-
-    
 }
 
 //实例化一个HttpClient类，设置其端口为3000
-var httpClient = new HttpClient(3000);
-
+var httpClient = new HttpClient();
+httpClient.start();
